08f6439558a628aaf86511cd91f28ee829997eeb
vladilima <vladilima1001@gmail.com>
2025-04-01 20:30:44 -0300

    feat: projeto do springboot

---
+++ projfabsoft-despesas/src/main/java/br/univille/projfabsoft_despesas/ProjfabsoftDespesasApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoft_despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftDespesasApplication.class, args);
+	}
+
+}
---
+++ projfabsoft-despesas/src/test/java/br/univille/projfabsoft_despesas/ProjfabsoftDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoft_despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
