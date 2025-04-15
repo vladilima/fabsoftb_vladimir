ac7f61a5d069217fc6659f7fb66098852856849b
vladilima <vladilima1001@gmail.com>
2025-04-15 19:13:42 -0300

    Fix: Faltou implementar o UsuarioServiceImpl

--- projfabsoft-despesas/src/main/java/br/univille/projfabsoft_despesas/service/impl/UsuarioServiceImpl.java
+++ projfabsoft-despesas/src/main/java/br/univille/projfabsoft_despesas/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,46 @@
 package br.univille.projfabsoft_despesas.service.impl;
 
-public class UsuarioServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.univille.projfabsoft_despesas.entity.Usuario;
+import br.univille.projfabsoft_despesas.repository.UsuarioRepository;
+import br.univille.projfabsoft_despesas.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        repository.save(usuario);
+        return usuario;
+    }
+
+    @Override
+    public List<Usuario> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Usuario getById(Long id) {
+        var retorno = repository.findById(id);
+        if(retorno.isPresent())
+            return retorno.get();
+        return null;
+    }
+
+    @Override
+    public Usuario delete(Long id) {
+        var usuario = getById(id);
+        if(usuario != null)
+            repository.deleteById(id);
+        return usuario;
+    }
 
 }
