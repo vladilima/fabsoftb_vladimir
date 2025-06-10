b2ae7e2561d534b6ad5da198fc08d2b3336e07f2
Vladimir de Lima <50593990+vladilima@users.noreply.github.com>
2025-06-10 00:55:54 +0000

    feat: adiciona tela que mostra usuarios e botao para outra tela

--- projfabsoft-frontend/src/app/usuario/usuario.component.ts
+++ projfabsoft-frontend/src/app/usuario/usuario.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UsuarioService } from '../service/usuario.service';
+import { Usuario } from '../model/usuario';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [HttpClientModule, CommonModule, RouterLink],
   templateUrl: './usuario.component.html',
-  styleUrl: './usuario.component.css'
+  styleUrl: './usuario.component.css',
+  providers: [UsuarioService]
 })
 export class UsuarioComponent {
+  listaUsuarios: Usuario[] = [];
 
+  constructor(private usuarioService : UsuarioService) {}
+
+  ngOnInit(){
+    console.log("Carregando Usuários...");
+    this.usuarioService.getUsuarios().subscribe(
+      usuarios => {
+        this.listaUsuarios = usuarios
+      }
+    );
+  }
 }
